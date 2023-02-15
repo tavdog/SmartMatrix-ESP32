e2ba8a6152057e3ffbe0382064c99f7abbb62d1a
Aiden Vigue <aiden@vigue.me>
2023-02-14 21:35:57 -0500

    move back to sync mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 //#define TIDBYT
-//#define MQTT_SSL
+#define MQTT_SSL
 
 #include <FS.h>
-#include "SPIFFS.h"
+#include "LittleFS.h"
 
 #include <Arduino.h>
 #include <WiFiManager.h>
@@ -14,7 +14,6 @@
 #include <WiFiClient.h>
 #endif
 
-#include <time.h>
 #include <webp/demux.h>
 
 #include <constants.h>
@@ -69,11 +68,12 @@ Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1234
 
 boolean newapplet = false;
 
-char hostName[80];
+char hostName[20];
 char applet_topic[22];
 char applet_rts_topic[26];
 char brightness_topic[22];
 char lastProgText[12];
+char statusAppletPath[30];
 
 WebPData webp_data;
 
@@ -82,6 +82,7 @@ int desiredBrightness = 20;
 int currentBrightness = 100;
 unsigned long bufferPos;
 bool recv_length = false;
+bool mqttShouldReconnect = true;
 
 #ifdef MQTT_SSL
 WiFiClientSecure wifiClient;
@@ -92,6 +93,7 @@ WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
 uint8_t *tmpbuf;
+uint8_t tempPixelBuffer[MATRIX_HEIGHT * MATRIX_WIDTH * 4];
 unsigned long bufsize;
 
 WebPDemuxer* demux;
@@ -100,75 +102,90 @@ uint32_t webp_flags;
 uint32_t current_frame = 1;
 uint32_t frame_count;
 
-//Frame buffers for animated WebP
-uint8_t * currentFrame;
+TaskHandle_t matrixTask;
+TaskHandle_t connectionTask;
 
-unsigned long mqtt_timeout_lastTime = 0;
 unsigned long last_frame_duration = 0;
 unsigned long last_frame_time = 0;
 unsigned long last_check_tsl_time = 0;
 unsigned long last_adjust_brightness_time = 0;
 
-void marqueeText(const String &buf, uint16_t color) {
-    dma_display.clearScreen();
-    dma_display.drawRect(0,0,MATRIX_WIDTH,MATRIX_HEIGHT,color);
-    int16_t x1, y1;
-    uint16_t w, h;
-    dma_display.getTextBounds(buf, 0, 0, &x1, &y1, &w, &h); //calc width of new string
-    dma_display.setCursor(32 - w / 2, 16 - h / 2);
-    dma_display.print(buf);
+void showStatusApplet(const char * statusApplet) {
+    sprintf(statusAppletPath, "/%s.webp", statusApplet);
+    if(!LittleFS.begin(true)){
+        return;
+    }
+
+    if(LittleFS.exists(statusAppletPath)) {
+        File file = LittleFS.open(statusAppletPath, FILE_READ);
+
+        //Set buffers
+        currentMode = NONE;
+        WebPDataClear(&webp_data);
+        WebPDemuxReleaseIterator(&iter);
+        WebPDemuxDelete(demux);
+
+        //setup webp buffer and populate from temporary buffer
+        webp_data.size = file.size();
+        webp_data.bytes = (uint8_t *) WebPMalloc(file.size());
+
+        //Fill buffer!
+        file.readBytes((char *)webp_data.bytes, file.size());
+        file.close();
+
+        //Show!
+        if (strncmp((const char*)webp_data.bytes, "RIFF", 4) == 0) {
+            newapplet = true;
+            currentMode = APPLET;
+        }
+    }
+
+    LittleFS.end();
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
     if (strcmp(topic, applet_topic) == 0) {
-        if(strncmp((char *)payload,"START",3) == 0) {
-            mqtt_timeout_lastTime = millis();
+        if(strncmp((char *)payload,"START",5) == 0) {
+            ArduinoOTA.end();
             recv_length = false;
             bufferPos = 0;
-
             client.publish(applet_rts_topic, "OK");
-        }else if(strncmp((char *)payload,"PING",4) == 0) {
+        } else if(strncmp((char *)payload,"PING",4) == 0) {
             client.publish(applet_rts_topic, "PONG");
         } else if(!recv_length) {
-            mqtt_timeout_lastTime = millis();
             bufsize = atoi((char *)payload);
             tmpbuf = (uint8_t *) malloc(bufsize);
             recv_length = true;
             client.publish(applet_rts_topic, "OK");
         } else {
             if(strncmp((char *)payload,"FINISH",6) == 0) {
-                mqtt_timeout_lastTime = 0;
                 if (strncmp((const char*)tmpbuf, "RIFF", 4) == 0) {
                     //Clear and reset all libwebp buffers.
                     WebPDataClear(&webp_data);
                     WebPDemuxReleaseIterator(&iter);
                     WebPDemuxDelete(demux);
 
-                    //Free the frame buffers if necessary.
-                    if(currentFrame != nullptr) {
-                        free(currentFrame);
-                    }
-
                     //setup webp buffer and populate from temporary buffer
                     webp_data.size = bufsize;
                     webp_data.bytes = (uint8_t *) WebPMalloc(bufsize);
+                    if(webp_data.bytes == NULL) {
+                        client.publish(applet_rts_topic, "DECODE_ERROR");
+                    } else {
+                        memcpy((void *)webp_data.bytes, tmpbuf, bufsize);
 
-                    memcpy((void *)webp_data.bytes, tmpbuf, bufsize);
-
-                    //free temporary buffer
+                        //set display flags!
+                        newapplet = true;
+                        currentMode = APPLET;
+                        client.publish(applet_rts_topic, "PUSHED");
+                    }
                     free(tmpbuf);
-
-                    //set display flags!
-                    newapplet = true;
-                    currentMode = APPLET;
-                    client.publish(applet_rts_topic, "PUSHED");
+                    ArduinoOTA.begin();
                 } else {
                     client.publish(applet_rts_topic, "DECODE_ERROR");
                 }
                 bufferPos = 0;
                 recv_length = false;
             } else {
-                mqtt_timeout_lastTime = millis();
                 memcpy((void *)(tmpbuf+bufferPos), payload, length);
                 bufferPos += length;
                 client.publish(applet_rts_topic, "OK");
@@ -177,23 +194,122 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
     }
 }
 
-
-void mqttReconnect() {
-    ESP.restart();
+void configModeCallback (WiFiManager *wm) {
+    showStatusApplet("wifi_config");
 }
 
-void configModeCallback (WiFiManager *wm) {
-    marqueeText("setup", dma_display.color565(0,255,255));
+void matrixLoop(void * parameter) {
+    for(;;) {
+        if (currentMode == APPLET) {
+            if(newapplet) {
+                demux = WebPDemux(&webp_data);
+                frame_count = WebPDemuxGetI(demux, WEBP_FF_FRAME_COUNT);
+                webp_flags = WebPDemuxGetI(demux, WEBP_FF_FORMAT_FLAGS);
+
+                newapplet = false;
+                current_frame = 1;
+            } else {
+                if(webp_flags & ANIMATION_FLAG) {
+                    if(millis() - last_frame_time > last_frame_duration) {
+                        if(WebPDemuxGetFrame(demux, current_frame, &iter)) {
+                            if(WebPDecodeRGBAInto(iter.fragment.bytes, iter.fragment.size, tempPixelBuffer, iter.width * iter.height * 4, iter.width * 4) != NULL) {
+                                int px = 0;
+                                for(int y = iter.y_offset; y < (iter.y_offset + iter.height); y++) {
+                                    for(int x = iter.x_offset; x < (iter.x_offset + iter.width); x++) {
+                                        //go pixel by pixel.
+                                        int pixelOffsetCF = ((y*MATRIX_WIDTH)+x)*3;
+                                        int pixelOffsetFT = px*4;
+
+                                        int alphaValue = tempPixelBuffer[pixelOffsetFT+3];
+                                        
+                                        if(alphaValue == 255) {
+                                            dma_display.writePixel(x,y, dma_display.color565(tempPixelBuffer[pixelOffsetFT],tempPixelBuffer[pixelOffsetFT+1],tempPixelBuffer[pixelOffsetFT+2]));
+                                        }
+                                        
+                                        px++;
+                                    }
+                                }
+
+                                last_frame_time = millis();
+                                last_frame_duration = iter.duration;
+                                current_frame++;
+                                if(current_frame > frame_count) {
+                                    current_frame = 1;
+                                }
+                            }
+                        } else {
+                            currentMode = NONE;
+                        }
+                    }
+                } else {
+                    //Static WebP
+                    if(WebPDecodeRGBInto(webp_data.bytes, webp_data.size, tempPixelBuffer, MATRIX_HEIGHT * MATRIX_WIDTH * 3, MATRIX_WIDTH * 3) != NULL) {
+                        
+                        for(int y = 0; y < MATRIX_HEIGHT; y++) {
+                            for(int x = 0; x < MATRIX_WIDTH; x++) {
+                                int pixBitStart = ((y*MATRIX_WIDTH)+x)*3;
+                                dma_display.writePixel(x,y, dma_display.color565(tempPixelBuffer[pixBitStart],tempPixelBuffer[pixBitStart+1],tempPixelBuffer[pixBitStart+2]));
+                            }
+                        }
+
+                        currentMode = NONE;
+                    }
+                }
+            }
+        }
+        vTaskDelay(100);
+    }
+}
+
+void connectionLoop(void * parameter) {
+    for(;;) {
+        if(WiFi.isConnected() && WiFi.getMode() == WIFI_STA) {
+            if(!client.connected()) {
+                showStatusApplet("mqtt_disconnected");
+                vTaskDelay(500);
+                if(mqttShouldReconnect) {
+                    showStatusApplet("mqtt_connecting");
+                    client.connect(hostName, MQTT_USERNAME, MQTT_PASSWORD);
+                    if (client.connected()) {
+                        showStatusApplet("mqtt_connected");
+                        vTaskDelay(500);
+                        client.subscribe(applet_topic);
+                        client.publish(applet_rts_topic, "DEVICE_BOOT");
+                        vTaskDelay(500);
+                        showStatusApplet("ready");
+                    }
+                } else {
+                    vTaskDelay(1000000);
+                }
+            }
+        } else {
+            showStatusApplet("wifi_connecting");
+            WiFi.reconnect();
+        }
+        vTaskDelay(5000);
+    }
 }
 
 void setup() {
     Serial.begin(115200);
+
     dma_display.begin();
     dma_display.setBrightness8(currentBrightness); //0-255
-    dma_display.setLatBlanking(1);
     dma_display.clearScreen();
-    marqueeText("SmartMx", dma_display.color565(0,255,255));
 
+    xTaskCreatePinnedToCore(
+      matrixLoop, /* Function to implement the task */
+      "MatrixTask", /* Name of the task */
+      3300,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      &matrixTask,  /* Task handle. */
+    1); /* Core where the task should run */
+
+    showStatusApplet("startup");
+
+    vTaskDelay(500);
+    
     #ifndef TIDBYT
     Wire.begin();
     if(!tsl.begin())
@@ -213,18 +329,17 @@ void setup() {
     sprintf(macFull, "%02X%02X%02X", baseMac[3], baseMac[4], baseMac[5]);
     snprintf(hostName, 11, PSTR("SmX-%s"),macFull);
 
+    showStatusApplet("wifi_connecting");
+
     WiFi.setHostname(hostName);
     WiFi.setAutoReconnect(true);
 
-    wifiManager.setTimeout(180);
+    wifiManager.setConnectTimeout(20);
+    wifiManager.setAPCallback(configModeCallback);
     wifiManager.setCleanConnect(true);
-
-    if (!wifiManager.autoConnect(hostName)) {
-        delay(3000);
-    }
+    wifiManager.autoConnect(hostName);
 
     ArduinoOTA.setHostname(hostName);
-    //ArduinoOTA.setPassword("SmX");
 
     ArduinoOTA.onStart([]() {
         currentMode = NONE;
@@ -234,34 +349,16 @@ void setup() {
         WebPDemuxReleaseIterator(&iter);
         WebPDemuxDelete(demux);
 
-        //Free the frame buffers if necessary.
-        if(currentFrame != nullptr) {
-            free(currentFrame);
-        }
+        mqttShouldReconnect = false;
+        client.disconnect();
 
-        marqueeText("OTA: Start", dma_display.color565(0,255,0));
-        delay(2500);
-    });
-    ArduinoOTA.onEnd([]() {
-        marqueeText("OTA: Done", dma_display.color565(0,255,0));
-        delay(2500);
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        char progText[12];
-        sprintf(progText, "OTA: %u%%", (progress / (total / 100)));
-
-        if(strcmp(progText, lastProgText) != 0) {
-            marqueeText(progText, dma_display.color565(255,255,0));
-            strcpy(lastProgText, progText);
-        }
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-        marqueeText("OTA: error", dma_display.color565(0,255,0));
-        delay(2500);
+        showStatusApplet("startup");
     });
 
     ArduinoOTA.begin();
 
+    esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+    sprintf(macFull, "%02X%02X%02X", baseMac[3], baseMac[4], baseMac[5]);
     snprintf_P(applet_topic, 22, PSTR("%s/%s/rx"), TOPIC_PREFIX, macFull);
     snprintf_P(applet_rts_topic, 26, PSTR("%s/%s/tx"), TOPIC_PREFIX, macFull);
 
@@ -270,34 +367,19 @@ void setup() {
     #endif
 
     client.setServer(MQTT_HOST, MQTT_PORT);
-    client.connect(hostName, MQTT_USERNAME, MQTT_PASSWORD);
     client.setCallback(mqttCallback);
 
-    if (client.connected()) {
-        client.subscribe(applet_topic);
-        client.publish(applet_rts_topic, "DEVICE_BOOT");
-    }
-
-    marqueeText("ready..", dma_display.color565(255,255,255));
+    xTaskCreatePinnedToCore(
+      connectionLoop, /* Function to implement the task */
+      "ConnectionTask", /* Name of the task */
+      3000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &connectionTask,  /* Task handle. */
+    0); /* Core where the task should run */
 }
 
 void loop() {
-    // Try to reconnect to wifi if connection lost
-    while (WiFi.status() != WL_CONNECTED || WiFi.localIP() == IPAddress(0,0,0,0)) {
-        WiFi.reconnect();
-        marqueeText("wi-fi_nc", dma_display.color565(255,0,0));
-        delay(10000);
-    }
-
-    client.loop();
-
-    ArduinoOTA.handle();
-
-    if (!client.connected()) {
-        marqueeText("mqtt_nc", dma_display.color565(255,0,0));
-        mqttReconnect();
-    }
-
     #ifndef TIDBYT
     //Update desired brightness
     if (millis() - last_check_tsl_time > 500) {
@@ -328,86 +410,6 @@ void loop() {
     }
     #endif
 
-    if (currentMode == WELCOME) {
-        currentMode = NONE;
-    }
-
-    if (currentMode == APPLET) {
-        if(newapplet) {
-            demux = WebPDemux(&webp_data);
-            frame_count = WebPDemuxGetI(demux, WEBP_FF_FRAME_COUNT);
-            webp_flags = WebPDemuxGetI(demux, WEBP_FF_FORMAT_FLAGS);
-
-            currentFrame = (uint8_t *) malloc(MATRIX_HEIGHT * MATRIX_WIDTH * 3);
-
-            newapplet = false;
-            current_frame = 1;
-        } else {
-            if(webp_flags & ANIMATION_FLAG) {
-                if(millis() - last_frame_time > last_frame_duration) {
-                    if(WebPDemuxGetFrame(demux, current_frame, &iter)) {
-                        uint8_t * fragmentTmp = (uint8_t *) malloc(iter.width*iter.height*4);
-                        if(WebPDecodeRGBAInto(iter.fragment.bytes, iter.fragment.size, fragmentTmp, iter.width * iter.height * 4, iter.width * 4) != NULL) {
-                            int px = 0;
-                            for(int y = iter.y_offset; y < (iter.y_offset + iter.height); y++) {
-                                for(int x = iter.x_offset; x < (iter.x_offset + iter.width); x++) {
-                                    //go pixel by pixel.
-                                    int pixelOffsetCF = ((y*MATRIX_WIDTH)+x)*3;
-                                    int pixelOffsetFT = px*4;
-
-                                    int alphaValue = fragmentTmp[pixelOffsetFT+3];
-                                    
-                                    if(alphaValue == 255) {
-                                        memcpy(currentFrame+pixelOffsetCF, fragmentTmp+pixelOffsetFT, 3);
-                                    }
-                                    
-                                    px++;
-                                }
-                            }
-
-                            //currentframe is good to send to screen now
-                            for(int y = 0; y < MATRIX_HEIGHT; y++) {
-                                for(int x = 0; x < MATRIX_WIDTH; x++) {
-                                    int pixBitStart = ((y*MATRIX_WIDTH)+x)*3;
-                                    dma_display.writePixel(x,y, dma_display.color565(currentFrame[pixBitStart],currentFrame[pixBitStart+1],currentFrame[pixBitStart+2]));
-                                }
-                            }
-
-                            last_frame_time = millis();
-                            last_frame_duration = iter.duration;
-                            current_frame++;
-                            if(current_frame > frame_count) {
-                                current_frame = 1;
-                            }
-                        }
-                        free(fragmentTmp);
-                    } else {
-                        currentMode = NONE;
-                    }
-                }
-            } else {
-                //Static WebP
-                uint8_t * tempPixelBuffer = (uint8_t *) malloc(MATRIX_HEIGHT * MATRIX_WIDTH * 3);
-                WebPDecodeRGBInto(webp_data.bytes, webp_data.size, tempPixelBuffer, MATRIX_HEIGHT * MATRIX_WIDTH * 3, MATRIX_WIDTH * 3);
-                
-                for(int y = 0; y < MATRIX_HEIGHT; y++) {
-                    for(int x = 0; x < MATRIX_WIDTH; x++) {
-                        int pixBitStart = ((y*MATRIX_WIDTH)+x)*3;
-                        dma_display.writePixel(x,y, dma_display.color565(tempPixelBuffer[pixBitStart],tempPixelBuffer[pixBitStart+1],tempPixelBuffer[pixBitStart+2]));
-                    }
-                }
-
-                free(tempPixelBuffer);
-
-                currentMode = NONE;
-            }
-        }
-    }
-
-    if(millis() - mqtt_timeout_lastTime > 3000 && mqtt_timeout_lastTime != 0) {
-        mqtt_timeout_lastTime = 0;
-        recv_length = false;
-        bufferPos = 0;
-        client.publish(applet_rts_topic, "TIMEOUT");
-    }
+    client.loop();
+    ArduinoOTA.handle();
 }
